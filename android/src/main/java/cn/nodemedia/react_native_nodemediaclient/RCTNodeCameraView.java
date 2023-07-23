587cba3a6913ce8d3e4c69881e11f5ece87ec2e6
Shauls <shaul919191@gmail.com>
2023-07-23 14:52:45 +0300

    bllop

--- android/src/main/java/cn/nodemedia/react_native_nodemediaclient/RCTNodeCameraView.java
+++ android/src/main/java/cn/nodemedia/react_native_nodemediaclient/RCTNodeCameraView.java
@@ -71,7 +71,7 @@ public class RCTNodeCameraView extends NodeCameraView implements LifecycleEventL
     }
 
 
-    public NodePublisher get(String outputUrl) {
+    public ReadableMap get(String outputUrl) {
     return mNodePublisher;
     }
 

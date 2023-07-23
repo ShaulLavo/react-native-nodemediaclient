013bc30e835a4dddee9f80d69cd88b835c677dee
Shauls <shaul919191@gmail.com>
2023-07-23 15:06:21 +0300

    bllop

--- android/src/main/java/cn/nodemedia/react_native_nodemediaclient/RCTNodeCameraView.java
+++ android/src/main/java/cn/nodemedia/react_native_nodemediaclient/RCTNodeCameraView.java
@@ -17,6 +17,7 @@ import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
 
